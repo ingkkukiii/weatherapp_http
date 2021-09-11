401ff95e5c98bbb2bee5c95388f32bda4f5f074e
hwanginkuk <kbighwang95@goole.com>
2021-09-11 21:33:10 +0900

    http weatherapp version 1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http_weather_demo/src/pages/loading_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Loading(),
+    );
+  }
+}
